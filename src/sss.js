c36cdf8e47eb7f0e1987d2bbffca9c2a6241d8a5
Sarala;git config --global user.emailsaralakumarigudimcherla2000@gmail.com <saralakumarigudimcherla2000@gmail.com>
2026-08-23 17:28:45 +0530

    Add form

--- src/sss.js
+++ src/sss.js
@@ -1,1 +1,23 @@
-console.log("SWAROOPA ");
+function App() {
+  return (
+    <div>
+      <h1>Student Form</h1>
+
+      <form>
+        <label>Name:</label>
+        <input type="text" />
+
+        <br /><br />
+
+        <label>Email:</label>
+        <input type="email" />
+
+        <br /><br />
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
